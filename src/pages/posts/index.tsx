6a87702c8e769e0a7a4fec73ee095d22651e5592
Vitor A Leal <vitorlealeng@gmail.com>
2022-02-09 13:08:27 -0300

    Added getServerSideProps to posts Page

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,24 @@
-const PostsPage = () => {
+import { GetServerSideProps, NextPage } from "next";
+
+type PostsPageProps = {
+  name: string;
+}
+
+const PostsPage: NextPage<PostsPageProps> = (props) => {
   return (
-    <h1>Posts!</h1>
+    <>
+      <h1>Posts!</h1>
+      <p>{props.name}</p>
+    </>
   );
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  //api
+  //ler arquivo
+  return {
+    props: {name: 'Nome', posts: [{title: 'Titulo do Post'}]}
+  }
 }
 
 export default PostsPage;
